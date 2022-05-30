9e5083b3727a0c18dbbefbfe7b4bfec5ee294cfa
Sadid369 <sadid.jones@gmail.com>
2022-05-30 20:40:47 +0600

    @ 30/05/2022 @2:09

--- server/index.js
+++ server/index.js
@@ -1,4 +1,29 @@
-console.log("Hello World");
+// IMPORTS FROM PACKAGE
 const express = require("express");
+// IMPORT FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const mongoose = require("mongoose");
 
-// CREATING AN API
+//INIT
+const PORT = 3000;
+const app = express();
+const DB =
+  "mongodb+srv://sadid:502797@cluster0.1np02dp.mongodb.net/?retryWrites=true&w=majority";
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+
+//Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
