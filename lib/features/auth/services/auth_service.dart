c66a6785c3c70749a40cd05fcdd3caf16397874d
Sadid369 <sadid.jones@gmail.com>
2022-06-02 20:14:07 +0600

    @ 2/6/2022 @ 3:22

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -73,14 +73,53 @@ class AuthService {
           response: res,
           context: context,
           onSuccess: () async {
-            // SharedPreferences prefs = await SharedPreferences.getInstance();
+            SharedPreferences prefs = await SharedPreferences.getInstance();
 
-            // Provider.of<UserProvider>(context, listen: false).setUser(res.body);
-            // await prefs.setString(
-            //     "x-auth-token", jsonDecode(res.body)["token"]);
-            // Navigator.pushNamedAndRemoveUntil(
-            //     context, HomeScreen.routeName, (route) => false);
+            Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+            await prefs.setString(
+                "x-auth-token", jsonDecode(res.body)["token"]);
+            Navigator.pushNamedAndRemoveUntil(
+                context, HomeScreen.routeName, (route) => false);
           });
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  //get user data
+  void getUserData(
+    BuildContext context,
+  ) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if (token == null) {
+        prefs.setString("x-auth-token", "");
+      }
+      var tokenRes = await http.post(
+        Uri.parse("$uri/tokenIsValid"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token!
+        },
+      );
+
+      var response = jsonDecode(tokenRes.body);
+
+      if (response == true) {
+        http.Response userRes = await http.get(
+          Uri.parse("$uri/"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'x-auth-token': token
+          },
+        );
+
+        var userProvider = Provider.of<UserProvider>(context, listen: false);
+        userProvider.setUser(userRes.body);
+        print(userProvider.user);
+      }
     } catch (e) {
       showSnackBar(context, e.toString());
     }
